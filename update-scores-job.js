1e85dc64e5da3c4f10606deb21f63c29f2d57659
Garrett Graham <gwadegraham@gmail.com>
2024-08-24 16:41:59 -0500

    Updating update-scores-job.js
    - updating job to only run on Sunday, since Heroku Scheduler doesn't have weekly option

--- update-scores-job.js
+++ update-scores-job.js
@@ -110,4 +110,11 @@ async function updateScores () {
     }
 }
 
-updateScores();
+const todayDate = new Date();
+
+if (todayDate.getDay() == 0) {
+    console.log("Today is Sunday, so games & scores are being updated.");
+    updateScores();
+} else {
+    console.log("Today is not Sunday, so games & scores are NOT being updated.");
+}
