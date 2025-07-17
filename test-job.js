c3b208ff03bbfb7242e2d2be63cee08b5ac793ef
Garrett Graham <gwadegraham@gmail.com>
2025-07-17 17:39:48 -0500

    Update test-job.js

--- test-job.js
+++ test-job.js
@@ -6,7 +6,8 @@ const todayDate = new Date();
 
 if (todayDate.getDay() == 0) {
     console.log("Today is Sunday, so games & scores are being updated.");
-    updateScores();
+    console.log("Current Date", todayDate.toLocaleString())
 } else {
     console.log("Today is not Sunday, so games & scores are NOT being updated.");
+    console.log("Current Date", todayDate.toLocaleString())
 }
