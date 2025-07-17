9cf7542fc7b7321963ba8516e4b74ea20e1e8770
Garrett Graham <gwadegraham@gmail.com>
2025-07-17 18:05:58 -0500

    Adding Nodemailer

--- test-job.js
+++ test-job.js
@@ -3,11 +3,41 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const todayDate = new Date();
+var dayText = "";
 
 if (todayDate.getDay() == 0) {
-    console.log("Today is Sunday, so games & scores are being updated.");
+    dayText = "Today is Sunday, so games & scores are being updated.";
+    console.log(dayText);
     console.log("Current Date", todayDate.toLocaleString())
 } else {
-    console.log("Today is not Sunday, so games & scores are NOT being updated.");
+    dayText = "Today is not Sunday, so games & scores are NOT being updated.";
+    console.log(dayText);
     console.log("Current Date", todayDate.toLocaleString())
 }
+
+let nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'gwadegraham@gmail.com',
+    pass: 'flgb pbdh dewn lhmw'
+  }
+});
+
+var emailMessage = "<h2>" + dayText + "</h2><br><p>Today's Date is: " + todayDate.toLocaleString() + "</p>";
+
+let mailOptions = {
+  from: 'gwadegraham@gmail.com',
+  to: 'garrett_graham@icloud.com',
+  subject: 'Sending Email using Node.js',
+  html: emailMessage
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
