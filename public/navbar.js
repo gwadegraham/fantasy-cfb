746faaa3705957b01ec9ff6a0f6d41513f0def25
Garrett Graham <gwadegraham@gmail.com>
2025-07-17 21:44:57 -0500

    Initial Campus Clash commit

--- public/navbar.js
+++ public/navbar.js
@@ -5,6 +5,9 @@ document.write('\
     <link  href="/../main.css">\
     <!-- Bootstrap CSS -->\
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer">\
+    <link rel="preconnect" href="https://fonts.googleapis.com">\
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\
+    <link href="https://fonts.googleapis.com/css2?family=Graduate&family=Pacifico&display=swap" rel="stylesheet">\
     <!-- JQuery -->\
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>\
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>\
@@ -12,7 +15,10 @@ document.write('\
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/js/bootstrap.min.js" integrity="sha512-UR25UO94eTnCVwjbXozyeVd6ZqpaAE9naiEUBK/A+QDbfSTQFhPGj5lOR6d8tsgbBk84Ggb5A3EkjsOgPRPcKA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>\
 </head>\
 <nav id="navbar" class="navbar">\
-    <div class="brand-title">Fantasy CFB</div>\
+    <div class="brand-title" style="display: flex; flex-flow: column;">\
+        <div style="font-family: Graduate, serif;font-weight: 400;font-style: normal;margin: auto;">CAMPUS</div>\
+        <div style="font-family: Pacifico, cursive;font-weight: 400;font-style: normal;margin: auto;margin-top: -18%;">Clash</div>\
+    </div>\
     <a href="#" class="toggle-button">\
         <span class="bar"></span>\
         <span class="bar"></span>\
