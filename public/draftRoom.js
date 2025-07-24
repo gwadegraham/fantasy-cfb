bf5de3beeac5fda5ace3e5b46f3bb883893c617d
Garrett Graham <gwadegraham@gmail.com>
2025-07-23 19:26:07 -0500

    Adding null error handling in Draft Room

--- public/draftRoom.js
+++ public/draftRoom.js
@@ -255,10 +255,11 @@ async function displayTeams(data) {
 
         if (team.seasons.length > 0) {
             var season = team.seasons.find((season) => season.season == (new Date().getFullYear() - 1));
+            var currentSeason = team.seasons.find((season) => season.season == (new Date().getFullYear()));
             conference = team.seasons.at(-1).conference;
             cumulScoreV1 = season != null ? season.cumulativeScoreV1 : "-";
             cumulScoreV2 = season != null ? season.cumulativeScoreV2 : "-";
-            expectedWins = (team.seasons.find((season) => season.season == (new Date().getFullYear())).expectedWins || 0);
+            expectedWins = currentSeason != null ? currentSeason.expectedWins : 0;
         }
 
         str += '<tr><td style="text-align: left;">';
