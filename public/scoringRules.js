a2a8df1e3771b26602eb843cba1ac7d7b5220bc5
Garrett Graham <gwadegraham@gmail.com>
2025-08-17 21:36:32 -0500

    Hiding admin functions from Scoring view and removing maintenance message

--- public/scoringRules.js
+++ public/scoringRules.js
@@ -5,3 +5,50 @@ var leagueCode;
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
+
+window.onload = async function() {
+    const response = await fetch(`/profile`, {
+        method: 'GET',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        }
+    });
+
+    response.json().then(async data => {
+        console.log("user metadata", data)
+
+        weekCode = window.localStorage.getItem("weekCode");
+        const currentSelectedWeek = window.localStorage.getItem("week");
+        if (currentSelectedWeek) {
+            $("#dropdownMenuButtonWeek").text(currentSelectedWeek);
+        } else {
+            $("#dropdownMenuButtonWeek").text("Week 1");
+            weekCode = window.localStorage.setItem("weekCode", "week-1");
+        }
+
+        leagueCode = window.sessionStorage.getItem("leagueCode");
+
+        if (leagueCode && (leagueCode != "undefined")) {
+            const currentSelectedLeague = window.sessionStorage.getItem("league");
+            if (currentSelectedLeague) {
+                $("#dropdownMenuButton").text(currentSelectedLeague);
+            }
+        } else {
+            var userLeague = data.user_metadata.metadata.league;
+            if (userLeague == "gg") {
+                leagueCode = "graham-league";
+            } else {
+                leagueCode = "claunts-league";
+            }
+        }
+        
+        var userRole = data.user_metadata.roles[0];
+        if (userRole != "Admin") {
+            document.querySelector('[admin-page]').remove();
+            document.querySelector('[league-selector]').remove();
+        } else if (userRole == "Admin") {
+            document.querySelector('.maintenance-container').style.display = 'none';
+        }
+    });
+  };
